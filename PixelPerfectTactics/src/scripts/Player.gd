9efcceea0b300d1b8592b1f2be379d47d4616fc9
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-08-04 13:10:52 +0400

    added movement

--- PixelPerfectTactics/src/scripts/Player.gd
+++ PixelPerfectTactics/src/scripts/Player.gd
@@ -1,7 +1,25 @@
 extends KinematicBody2D
 
+export var speed: Vector2
+var velocity: Vector2 = Vector2(0, 0)
+
 
 func _ready() -> void:
 	print("ready")
+	
 
+func _physics_process(delta: float) -> void:
+	velocity = calculate_velocity(speed) * delta
+	velocity = move_and_slide(velocity, Vector2.ZERO)
+
+
+func calculate_velocity(spd) -> Vector2:
+	var return_velocity: Vector2 = velocity
+	
+	return_velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	return_velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	return_velocity *= spd
+	
+	return return_velocity
 
