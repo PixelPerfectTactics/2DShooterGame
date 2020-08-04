16537ca01f67d3ea24a5ab190b6f47c31aa93c8f
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-08-04 15:23:24 +0400

    added sprint

--- 2DShooterGame/src/scripts/Player.gd
+++ 2DShooterGame/src/scripts/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 export var speed: Vector2
+export var sprint_boost: Vector2
 var velocity: Vector2 = Vector2(0, 0)
 
 
@@ -9,17 +10,20 @@ func _ready() -> void:
 	
 
 func _physics_process(delta: float) -> void:
-	velocity = calculate_velocity(speed) * delta
+	velocity = calculate_velocity(speed, sprint_boost) * delta
 	velocity = move_and_slide(velocity, Vector2.ZERO)
 
 
-func calculate_velocity(spd) -> Vector2:
+func calculate_velocity(spd, boost) -> Vector2:
 	var return_velocity: Vector2 = velocity
 	
 	return_velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	return_velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
-	return_velocity *= spd
+	if Input.is_action_pressed("sprint"):
+		return_velocity *= spd + boost
+	else:
+		return_velocity *= spd
 	
 	return return_velocity
 
